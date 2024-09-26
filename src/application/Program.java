5145c8e397f2a80679bfd964ec4ff7e011660460
Felipe Moreira <felipe0365@gmail.com>
2024-09-26 15:03:57 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department department = new Department(1, "books");
 
-        System.out.println(department);
+        Seller seller = new Seller(21, "bob", "bob@gmail.com", 3000.0, new Date(), department);
+
+        System.out.println(seller);
     }
 }
