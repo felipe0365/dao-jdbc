b0773bb829233127f7d4cadf3141962d42f590a6
Felipe Moreira <felipe0365@gmail.com>
2024-09-27 09:52:24 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,5 +34,11 @@ public class Program {
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com",4000.0 , new Date(), department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller  = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+
     }
 }
